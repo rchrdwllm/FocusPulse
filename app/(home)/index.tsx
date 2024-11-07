6d70bc6b7c8651333254e74ef82980cfebdc8ba6
Richard William <rwilliam.flores@gmail.com>
2024-11-08 01:15:34 +0800

    Initialized timer screen

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,11 +1,34 @@
-import { Text } from "@/components/Text";
-import React from "react";
+import { H1 } from "@/components/Typography";
 import SafeAreaWrapper from "@/components/SafeAreaWrapper";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { View } from "react-native";
+import { colors } from "@/constants/colors";
 
 const HomeScreen = () => {
   return (
     <SafeAreaWrapper className="bg-background px-4">
-      <Text>HomeScreen</Text>
+      <View className="flex-1 justify-center items-center">
+        <CountdownCircleTimer
+          isPlaying
+          duration={1250}
+          colors={[colors.dark.primary] as any}
+          trailColor={colors.dark.secondary as any}
+          size={275}
+        >
+          {({ remainingTime }) => {
+            const minutes = Math.floor(remainingTime / 60);
+            const seconds = remainingTime % 60;
+
+            return (
+              <View className="bg-[#262138] h-[252px] w-[252px] rounded-full justify-center items-center">
+                <H1 className="text-white text-7xl">
+                  {minutes}:{seconds}
+                </H1>
+              </View>
+            );
+          }}
+        </CountdownCircleTimer>
+      </View>
     </SafeAreaWrapper>
   );
 };
--- constants/colors.ts
+++ constants/colors.ts
@@ -1,8 +1,14 @@
 export const colors = {
   light: {
     background: "#ffffff",
+    primary: "#594EFC",
+    secondary: "#615884",
+    tertiary: "#FFFFFF",
   },
   dark: {
     background: "#1e1a2d",
+    primary: "#594EFC",
+    secondary: "#352F5E",
+    tertiary: "#262138",
   },
 };
