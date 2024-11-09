b7eb6ff6a7048a55e25e31b39a5b9a96ae3b1ded
Richard William <rwilliam.flores@gmail.com>
2024-11-10 02:36:02 +0800

    Removed config

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,5 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
-const {
-  wrapWithReanimatedMetroConfig,
-} = require("react-native-reanimated/metro-config");
 
-module.exports = wrapWithReanimatedMetroConfig(
-  withNativeWind(config, { input: "./global.css" })
-);
+module.exports = withNativeWind(config, { input: "./global.css" });
