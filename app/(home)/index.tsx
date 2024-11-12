e4f148b94482772c457e5e9e345b457d8b370fb4
Richard William <rwilliam.flores@gmail.com>
2024-11-13 00:46:29 +0800

    Initialized task cards

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -5,7 +5,10 @@ import { View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { TimerState } from "@/types";
-import { H1 } from "@/components/ui/typography";
+import { H1, H3 } from "@/components/ui/typography";
+import { FlatList } from "react-native";
+import { Text } from "@/components/ui/text";
+import Task from "@/components/tasks/task";
 
 const HomeScreen = () => {
   const {
@@ -24,32 +27,43 @@ const HomeScreen = () => {
   }, [task]);
 
   return (
-    <SafeAreaWrapper className="bg-background px-4">
-      <View className="flex-1 justify-center items-center gap-12">
-        <View className="w-full items-center px-12 gap-12">
-          <H1 className="text-center text-3xl">
-            {timerState === "focus"
-              ? "Focus!"
-              : timerState === "short"
-              ? "Take a short break"
-              : "Enjoy a long break"}
-          </H1>
-          <Input
-            value={currentTask}
-            onChange={(e) => setCurrentTask(e.nativeEvent.text)}
-            className="w-full"
-          />
-        </View>
-        <CountdownTimer
-          newTimerKey={parseInt(newTimerKey as string)}
-          focusDuration={parseInt(focusDuration as string)}
-          shortDuration={parseInt(shortDuration as string)}
-          longDuration={parseInt(longDuration as string)}
-          sessions={parseInt(sessions as string)}
-          timerState={timerState}
-          setTimerState={setTimerState}
-        />
-      </View>
+    <SafeAreaWrapper className="bg-background">
+      <FlatList
+        ListHeaderComponent={() => (
+          <View className="pt-12 pb-8 justify-center items-center gap-12">
+            <View className="w-full items-center px-12 gap-12">
+              <H1 className="text-center text-3xl">
+                {timerState === "focus"
+                  ? "Focus!"
+                  : timerState === "short"
+                  ? "Take a short break"
+                  : "Enjoy a long break"}
+              </H1>
+              <Input
+                value={currentTask}
+                onChange={(e) => setCurrentTask(e.nativeEvent.text)}
+                className="w-full"
+              />
+            </View>
+            <CountdownTimer
+              newTimerKey={parseInt(newTimerKey as string)}
+              focusDuration={parseInt(focusDuration as string)}
+              shortDuration={parseInt(shortDuration as string)}
+              longDuration={parseInt(longDuration as string)}
+              sessions={parseInt(sessions as string)}
+              timerState={timerState}
+              setTimerState={setTimerState}
+            />
+            <View className="items-center gap-2">
+              <H3>Tasks</H3>
+              <Text className="text-muted">You can view your tasks below!</Text>
+            </View>
+          </View>
+        )}
+        data={["1", "2", "3", "4", "5"]}
+        renderItem={({ item }) => <Task />}
+        className="flex-1 px-4"
+      />
     </SafeAreaWrapper>
   );
 };
---
+++ components/tasks/task.tsx
@@ -0,0 +1,20 @@
+import { useColorScheme, View } from "react-native";
+import { H3 } from "../ui/typography";
+import { Text } from "../ui/text";
+import { CircleCheckBig } from "lucide-react-native";
+import { colors } from "@/constants/colors";
+
+const Task = () => {
+  const colorScheme = useColorScheme() || "light";
+  const { muted } = colors[colorScheme];
+
+  return (
+    <View className="bg-secondary p-4 rounded-2xl my-2 flex-row gap-4">
+      <CircleCheckBig color={muted} />
+      <H3 className="text-lg flex-1">Task title</H3>
+      <Text className="text-muted">1/2</Text>
+    </View>
+  );
+};
+
+export default Task;
