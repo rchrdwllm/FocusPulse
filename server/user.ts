9b087b3bac097f3a29d2c248a362c676396c15d8
Richard William <rwilliam.flores@gmail.com>
2024-11-23 17:20:40 +0800

    Updated fetching logic

--- server/user.ts
+++ server/user.ts
@@ -1,7 +1,69 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateEmail, updateProfile } from "firebase/auth";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 export const getCurrentUser = () => {
   const auth = getAuth();
 
   return auth.currentUser;
+};
+
+export const updateBio = async (bio: string) => {
+  const currentUser = getCurrentUser();
+
+  if (!currentUser) {
+    return { error: "No user logged in" };
+  }
+
+  try {
+    const userRef = doc(db, "userBios", currentUser.uid);
+    const userDoc = await getDoc(userRef);
+
+    if (!userDoc.exists()) {
+      try {
+        await setDoc(userRef, { bio, id: currentUser.uid }, { merge: true });
+
+        return { success: "Bio updated" };
+      } catch (error) {
+        return { error };
+      }
+    }
+
+    await updateDoc(userRef, { bio });
+
+    return { success: "Bio updated" };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const updateOtherDetails = async ({
+  name,
+  email,
+  image,
+}: {
+  name: string;
+  email: string;
+  image: string | null;
+}) => {
+  const currentUser = getCurrentUser();
+
+  if (!currentUser) {
+    return { error: "No user logged in" };
+  }
+
+  try {
+    const auth = getAuth();
+
+    await updateProfile(auth.currentUser!, {
+      displayName: name,
+      photoURL: image,
+    });
+
+    currentUser.reload();
+
+    return { success: { name, image } };
+  } catch (error) {
+    return { error };
+  }
 };
